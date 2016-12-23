a1c4a28612c4c4b95a6b5b6ae6e313396bfe05c7
Thiyagesh\Thiyagesh S <thiyageshwaran.s@inxsasia.com>
2016-12-23 15:32:53 +0530

    thiyagesh check

--- app/src/main/java/uiexamples/msf/com/uiandroidexamples/uiactivities/MainActivity.java
+++ app/src/main/java/uiexamples/msf/com/uiandroidexamples/uiactivities/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
+        //commend added
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
